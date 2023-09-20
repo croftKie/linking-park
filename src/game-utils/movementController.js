a765b8837923eae56dfb499ddb4beef332e5257d
Kieran Croft <croftkie@proton.me>
2023-09-20 23:13:54 +0100

    movement controller conditionals added to reduce duplicate movement

--- src/game-utils/movementController.js
+++ src/game-utils/movementController.js
@@ -1,43 +1,79 @@
 export function movementController(player) {
   onKeyDown("w", () => {
+    if (isKeyDown("a") || isKeyDown("s") || isKeyDown("d")) {
+      return;
+    }
     player.move(0, -150);
   });
   onKeyPress("w", () => {
+    if (isKeyDown("a") || isKeyDown("s") || isKeyDown("d")) {
+      return;
+    }
     player.play("run_up");
   });
   onKeyRelease("w", () => {
+    if (isKeyDown("a") || isKeyDown("s") || isKeyDown("d")) {
+      return;
+    }
     player.play("idle_up");
   });
   //down movement
   onKeyDown("s", () => {
+    if (isKeyDown("w") || isKeyDown("a") || isKeyDown("d")) {
+      return;
+    }
     player.move(0, 150);
   });
   onKeyPress("s", () => {
+    if (isKeyDown("w") || isKeyDown("a") || isKeyDown("d")) {
+      return;
+    }
     player.play("run_down");
   });
   onKeyRelease("s", () => {
+    if (isKeyDown("w") || isKeyDown("a") || isKeyDown("d")) {
+      return;
+    }
     player.play("idle_down");
   });
   // left movement
   onKeyDown("a", () => {
+    if (isKeyDown("w") || isKeyDown("s") || isKeyDown("d")) {
+      return;
+    }
     player.move(-150, 0);
     player.flipX = true;
   });
   onKeyPress("a", () => {
+    if (isKeyDown("w") || isKeyDown("s") || isKeyDown("d")) {
+      return;
+    }
     player.play("run_side");
   });
   onKeyRelease("a", () => {
+    if (isKeyDown("w") || isKeyDown("s") || isKeyDown("d")) {
+      return;
+    }
     player.play("idle_side");
   });
   // right movement
   onKeyDown("d", () => {
+    if (isKeyDown("w") || isKeyDown("s") || isKeyDown("a")) {
+      return;
+    }
     player.move(150, 0);
     player.flipX = false;
   });
   onKeyPress("d", () => {
+    if (isKeyDown("w") || isKeyDown("s") || isKeyDown("a")) {
+      return;
+    }
     player.play("run_side");
   });
   onKeyRelease("d", () => {
+    if (isKeyDown("w") || isKeyDown("s") || isKeyDown("a")) {
+      return;
+    }
     player.play("idle_side");
   });
 }
